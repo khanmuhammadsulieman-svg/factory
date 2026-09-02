eae20fd497b3c2f35de9ef9dfaf778b5e6648f26
Sulieman khan <khanmuhammadsulieman@gmail.com>
2026-09-02 11:24:02 +0500

    Add useEffect import to CheckoutPage component

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import { CheckCircle2, HandCoins, CreditCard } from 'lucide-react';
